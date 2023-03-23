d0fbc428a7d1d0815539851369a2ec3e82cd5521
Ridho Suhaebi Arrowi <94096357+Ridhorezi@users.noreply.github.com>
2023-03-23 22:18:09 +0700

    Create category screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,12 +1,79 @@
+import 'package:desayur/services/utils.dart';
+import 'package:desayur/widgets/categories_widget.dart';
+import 'package:desayur/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+  CategoriesScreen({super.key});
+
+  final List<Color> gridColors = [
+    const Color(0xff53b175),
+    const Color(0xffF8A44C),
+    const Color(0xffF7A593),
+    const Color(0xffD3B0E0),
+    const Color(0xffB7DFF5),
+    const Color(0xffB7DF77),
+  ];
+
+  List<Map<String, dynamic>> catInfo = [
+    {
+      'imgPath': 'assets/images/cat/fruits.png',
+      'catText': 'Fruits',
+    },
+    {
+      'imgPath': 'assets/images/cat/veg.png',
+      'catText': 'Vegetables',
+    },
+    {
+      'imgPath': 'assets/images/cat/Spinach.png',
+      'catText': 'Herbs',
+    },
+    {
+      'imgPath': 'assets/images/cat/nuts.png',
+      'catText': 'Nuts',
+    },
+    {
+      'imgPath': 'assets/images/cat/spices.png',
+      'catText': 'Spices',
+    },
+    {
+      'imgPath': 'assets/images/cat/grains.png',
+      'catText': 'Grains',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Categories screen')),
+    final utils = Utils(context);
+    Color color = utils.color;
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 5,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        title: TextWidget(
+          text: 'Categories',
+          color: color,
+          textsize: 24,
+          isTitle: true,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: 240 / 250,
+          crossAxisSpacing: 30,
+          mainAxisSpacing: 30,
+          // itemCount: passedColors.length,
+          children: List.generate(6, (index) {
+            return CategoriesWidget(
+              catText: catInfo[index]['catText'],
+              imgPath: catInfo[index]['imgPath'],
+              passedColor: gridColors[index],
+            );
+          }),
+        ),
+      ),
     );
   }
 }
